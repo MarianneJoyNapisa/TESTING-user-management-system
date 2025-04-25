037d3ab473e2ca699923245259159599095ca189
MarianneJoyNapisa <mariannejoynapisa@gmail.com>
2025-04-26 02:01:50 +0800

    Initial commit frontent-profile-admin-fake-backend

--- frontend-signup-auth/src/app/_helpers/index.ts
+++ frontend-signup-auth/src/app/_helpers/index.ts
@@ -2,5 +2,5 @@ export * from './app.initializer';
 export * from './auth.guard';
 export * from './error.interceptor';
 export * from './jwt.interceptor';
-//  no fake-backend';
+export * from './fake-backend';
 export * from './must-match.validator';
